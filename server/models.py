2c98a1b95918f75bfadf4ea4222baaf406f2990b
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-07-03 18:17:33 +0300

    adds serialization to final commit

--- server/models.py
+++ server/models.py
@@ -11,8 +11,10 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -28,8 +30,11 @@ class Customer(db.Model):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+
+    serialize_rules = ('-reviews.item',)
+
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -41,8 +46,10 @@ class Item(db.Model):
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
     
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
         __tablename__="reviews"
+
+        serialize_rules= ('-customer.reviews','-item.reviews')
 
         id = db.Column(db.Integer, primary_key=True)
         comment = db.Column(db.String)
